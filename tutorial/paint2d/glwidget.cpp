27494e25bba3c8d7dc073211286b849d1924a086
Okai Addy <Okai Addy@localhost>
2015-10-02 09:26:19 -0700

    added opengl

--- tutorial/paint2d/glwidget.cpp
+++ tutorial/paint2d/glwidget.cpp
@@ -1,7 +1,28 @@
 #include "glwidget.h"
 
+#include <QGLFormat>
+#include <qtimer.h>
+
+#include "helper.h"
+
 GLWidget::GLWidget(Helper *helper, QWidget *parent)
 	: QGLWidget(QGLFormat(QGL::SampleBuffers), parent), helper(helper)
 {
+    setFixedSize(200, 200);
+    setAutoFillBackground(false);
 }
 
+void GLWidget::paintEvent(QPaintEvent *event)
+{
+    QPainter painter;
+    painter.begin(this);
+    painter.setRenderHint(QPainter::Antialiasing);
+    helper->paint(&painter, event, elapsed);
+    painter.end();
+}
+
+void GLWidget::animate()
+{
+    elapsed = (elapsed + qobject_cast<QTimer*>(sender())->interval()) % 1000;
+    update();
+}
