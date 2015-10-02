dfb70e5619f3788df4a6c9cfb7713dde809120e6
Okai Addy <noaddy@stanford.edu>
2015-10-02 10:21:26 -0700

    tga

--- game/lib/src/tga.cpp
+++ game/lib/src/tga.cpp
@@ -39,15 +39,15 @@ void TGA_Texture(GLuint textureArray[], const char* strFileName, int ID)
 	return;
 }
 
-/*ImageTGA* load_TGA(const char* strfilename)
+ImageTGA* load_TGA(const char* strfilename)
 {
 	ImageTGA *pImgData	= NULL;
 	FILE *pFile			= NULL;
-	WORD width			= 0;
-	WORD height			= 0;
-	byte length			= 0;
-	byte imgType		= 0;
-	byte bits			= 0;
+	short width			= 0;
+	short height			= 0;
+	quint8 length			= 0;
+	quint8 imgType		= 0;
+	quint8 bits			= 0;
 	int channels		= 0;
 	int stride			= 0;
 	int i				= 0;
@@ -66,13 +66,13 @@ void TGA_Texture(GLuint textureArray[], const char* strFileName, int ID)
 	}
 	
 	pImgData = (ImageTGA*)malloc(sizeof(ImageTGA));
-	fread(&length, sizeof(byte), 1, pFile);
+	fread(&length, sizeof(quint8), 1, pFile);
 	fseek(pFile, 1, SEEK_CUR);
-	fread(&imgType, sizeof(byte), 1, pFile);
+	fread(&imgType, sizeof(quint8), 1, pFile);
 	fseek(pFile, 9, SEEK_CUR);
-	fread(&width, sizeof(WORD), 1, pFile);
-	fread(&height, sizeof(WORD), 1, pFile);
-	fread(&bits, sizeof(byte), 1, pFile);	
+	fread(&width, sizeof(short), 1, pFile);
+	fread(&height, sizeof(short), 1, pFile);
+	fread(&bits, sizeof(quint8), 1, pFile);	
 	fseek(pFile, length+1, SEEK_CUR);	
 	
 	if(imgType != TGA_RLE)
@@ -125,24 +125,24 @@ void TGA_Texture(GLuint textureArray[], const char* strFileName, int ID)
 	}
 	else
 	{
-		byte rleID = 0;
+		quint8 rleID = 0;
 		int colorsRead = 0;
 		channels = bits/8;
 		stride = channels*width;
 		
 		pImgData->data = new unsigned char[stride*height];
-		byte *pColors = new byte[channels];
+		quint8 *pColors = new quint8[channels];
 		
 		while(i<width*height)
 		{
-			fread(&rleID, sizeof(byte), 1, pFile);
+			fread(&rleID, sizeof(quint8), 1, pFile);
 			
 			if(rleID < 128)
 			{
 				rleID++;
 				while(rleID)
 				{
-					fread(pColors, sizeof(byte)*channels, 1, pFile);
+					fread(pColors, sizeof(quint8)*channels, 1, pFile);
 					
 					pImgData->data[colorsRead+0] = pColors[2];
 					pImgData->data[colorsRead+1] = pColors[1];
@@ -158,7 +158,7 @@ void TGA_Texture(GLuint textureArray[], const char* strFileName, int ID)
 			else
 			{
 				rleID -= 127;
-				fread(pColors, sizeof(byte)*channels, 1, pFile);
+				fread(pColors, sizeof(quint8)*channels, 1, pFile);
 				
 				while(rleID)
 				{
@@ -182,32 +182,5 @@ void TGA_Texture(GLuint textureArray[], const char* strFileName, int ID)
 	pImgData->size_y	= height;
 	
 	return pImgData;
-}*/
-
-ImageTGA* loadTGA(QString filename)
-{
-    ImageTGA *pImgData	= NULL;
-    int channels		= 0;
-    int stride			= 0;
-    int i				= 0;
-    static int timecalled=0;
-    char angletext[100];
-    char tempString[128];
-
-    sprintf(angletext, "%i", timecalled++);
-
-    QImage image(filename);
-    if(image.isNull())
-    {
-//        sprintf(tempString, "Error loading tga file: %s", strfilename);
-//        QErrorMessage::showMessage(QString("Error loading tga file: %s").arg(filename));
-        //MessageBox(NULL, tempString, "Error", MB_OK);
-        //MessageBox(NULL, angletext, "ERROR", MB_OK);
-        return NULL;
-    }
-
-    pImgData = (ImageTGA*)malloc(sizeof(ImageTGA));
-
-
-    return pImgData;
 }
+
